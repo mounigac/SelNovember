21f3631d1e5855020de08c118407ad897810d338
TestLeafPages <sarath.muthuswamy@testleaf.com>
2019-12-08 13:24:50 +0530

    week 3 code

--- week3/day1/BMW.java
+++ week3/day1/BMW.java
@@ -1,8 +1,10 @@
 package week3.day1;
 
 public class BMW extends Car {
+//	public final int model = 300;
 	public void sunRoof() {
 		System.out.println("Sun Roof");
+		
 			}
 	public void applyBrake() {
 		System.out.println("Apply Brake - ABS Brake");
--- week3/day1/Vehicle.java
+++ week3/day1/Vehicle.java
@@ -1,7 +1,7 @@
 package week3.day1;
 
 public class Vehicle {
-	public void applyBrake() {
+	public  void applyBrake() {
 		System.out.println("Apply Brake - Normal Brake");
 	}
 	public void soundHorn() {
